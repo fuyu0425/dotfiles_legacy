4998ec4f0945cb9d75457ae81ad9659c2da05eaa
傅裕夫 <a0919610611@gmail.com>
2016-07-13 23:37:36 +0800

    vim settings

--- auto_install.sh
+++ auto_install.sh
@@ -2,10 +2,15 @@
 echo "OS is $(uname -s)"
 echo "start dotfile install"
 here=$PWD
+echo "vim setting"
+cd .vim/bundle/YouCompleteMe
+./install.py --clang-completer
+vim +PluginInstall +qall
 #echo "$PWD"
 echo "the dotfile location is $here"
 cd ~
 echo "create symbolic link in $PWD"
 ln -s $here/.vimrc
 ln -s $here/.zshrc
+ln -s $here/.vim
 echo "complete"
