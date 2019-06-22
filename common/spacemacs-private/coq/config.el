bf870b285f763c86fcdf388edc53a95992903c9a
a0919610611 <a0919610611@gmail.com>
2019-06-23 00:07:07 +0800

    add coq

---
+++ common/spacemacs-private/coq/config.el
@@ -0,0 +1,16 @@
+;;; config.el --- Coq Layer configuration File for Spacemacs
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Olivier Verdier <olivier.verdier@gmail.com>
+;; URL: https://github.com/olivierverdier/spacemacs-coq
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defvar proof-general-path "~/source/PG/generic/proof-site"
+  "The path to proof general")
+(defvar ssreflect-path "~/source/math-comp/mathcomp/ssreflect/pg-ssr"
+  "The path to ssreflect")
+
